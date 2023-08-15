706c1080899c80ee7593fd9935e0a9a1c5461272
Sanket-08 <sanketpatil0820@gmail.com>
2023-08-15 18:14:25 +0530

    first commit

---
+++ src/products.js
@@ -0,0 +1,94 @@
+import product1 from "./assets/products/1.webp";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.jpeg";
+import product6 from "./assets/products/6.png";
+import product7 from "./assets/products/7.png";
+import product8 from "./assets/products/8.png";
+import product9 from "./assets/products/9.png";
+import product10 from "./assets/products/10.png";
+import product11 from "./assets/products/11.webp";
+import product12 from "./assets/products/12.webp";
+import product13 from "./assets/products/13.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "CAP",
+    price: 40.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Speaker",
+    price: 100.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Watch",
+    price: 40.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "shoe",
+    price: 40.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Shoe",
+    price: 40.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "IPhone",
+    price: 999.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Macbook Pro 2022 (M1)",
+    price: 1999.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Cannon M50 Camera",
+    price: 699.0,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "WLS Van Gogh Denim Jacket",
+    price: 228.0,
+    productImage: product9,
+  },
+  {
+    id: 10,
+    productName: "LED Light Strips",
+    price: 19.99,
+    productImage: product10,
+  },
+  {
+    id: 11,
+    productName: "SPECTRUM LS TEE",
+    price: 68.0,
+    productImage: product11,
+  },
+  {
+    id: 12,
+    productName: "AUTO SERVICE SHIRT by GOLF WANG",
+    price: 120.0,
+    productImage: product12,
+  },
+  {
+    id: 13,
+    productName: "DON'T TRIP UNSTRUCTURED HAT",
+    price: 40.0,
+    productImage: product13,
+  },
+];
